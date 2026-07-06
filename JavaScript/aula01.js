5609e4d70b5d028d554fd7313e8c1891f9b19eca
ArturYouX <arthur.souza-youxlab@institutoyoux.org.br>
2026-07-06 17:16:09 -0300

    adicionando estudo de HTML, CSS e JavaScript

---
+++ JavaScript/aula01.js
@@ -0,0 +1,3 @@
+"use strict";
+let nome="Artur"
+console.log("Nome: " + nome)
---
+++ JavaScript/aula03.js
@@ -0,0 +1,9 @@
+"use strict"
+
+
+let nome = "Artur"
+nome = "ArturSouza"
+nome = 64
+const curso="JavaScript"
+curso="HTML"
+console.log(curso)
