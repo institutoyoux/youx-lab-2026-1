831b3899d4d85f45c50f91db703b1b06b9f6d9fe
KauanMartins496 <kauan.martins-youxlab@institutoyoux.org.br>
2026-07-07 16:52:47 -0300

    Aula 46 e Exercicios 44

--- postgresql/aulas.sql
+++ postgresql/aulas.sql
@@ -821,3 +821,59 @@ group by pdt.nome
 	  end 
 from uf -- Serve tipo o if com print do pythonn
 
+-- EXERCICIOS 	
+--1. O nome do cliente e somente o mês de nascimento. Caso a data de nascimento não esteja preenchida mostrar a mensagem “Não informado”.
+select nome,
+      coalesce(extract(month from data_nascimento),'0') as Mes from cliente;
+	
+--2. O nome do cliente e somente o nome do mês de nascimento (Janeiro, Fevereiro etc). Caso a data de nascimento não esteja preenchida mostrar a mensagem “Não informado”.
+select nome,case extract(month from data_nascimento)
+            when 1 then 'Janeiro'
+			when 2 then 'Fevereiro'
+	        when 3 then 'Marco'
+			when 4 then 'Abril'
+			when 5 then 'Maio'
+			when 6 then 'Junho'
+			when 7 then 'Julho'
+			when 8 then 'Agosto'
+			when 9 then 'Setembro'
+			when 10 then 'Outubro'
+			when 11 then 'Novembro'
+			when 12 then 'Dezembro'
+		    else 'nao informado'
+			end as mes
+		from cliente;
+
+--3. O nome do cliente e somente o ano de nascimento. Caso a data de nascimento não esteja preenchida mostrar a mensagem “Não informado”
+select nome,
+      coalesce(extract(year from data_nascimento),'0') as Ano from cliente;
+
+--4. caractere 5 até o caractere 10 de todos os municípios.
+select nome,substring(nome,5,10) from municipio;
+
+--5. O nome de todos os municípios em letras maiúsculas.
+select nome,upper(nome) from municipio;
+
+--6. O nome do cliente e o gênero. Caso seja M mostrar “Masculino”, senão mostrar “Feminino”.
+select nome,case genero 
+           when 'M' then 'Masculino'
+		   else 'Feminino'
+		   end 
+		   from cliente
+
+--7. O nome do produto e o valor. Caso o valor seja maior do que R$ 500,00 mostrar a mensagem “Acima de 500”, caso contrário, mostrar a mensagem “Abaixo de 500”.
+select nome,valor, case when valor >= 500 then 'Acima ou igual a 500 reais'
+                  else 'Abaixo de 500 Reais'
+				  end
+				  from produto
+
+-- SUBCONSULTAS 
+select data_pedido,valor_total from pedidos where valor_total > (select avg(valor_total) from pedidos) -- Seleciona a data,e os valores APENAS que estao acima da media
+
+-- Exemplo com count 
+select pdd.data_pedido,
+       pdd.valor_total,
+	   (select sum(quantidade) from pedido_produto pdp where pdp.idpedido = pdd.idpedido)
+	   from pedidos pdd
+
+-- Exemplo com update 
