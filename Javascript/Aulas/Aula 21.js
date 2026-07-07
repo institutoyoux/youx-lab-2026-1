6c3e5b3fbc50be55930a84be23ba4ab6e5d6acbc
YouX <leonardo.alvarenga-youxlab@institutoyoux.org.br>
2026-07-07 16:43:46 -0300

    Aula 22 Javascript

--- Javascript/Aulas/Aula 21.js
+++ Javascript/Aulas/Aula 21.js
@@ -1,6 +1,6 @@
 function canal(){
     let n1=1
-    let n2=3    
+    let n2=3
     let res=n1*n2
     if(res%2 == 0)
         return "Par"
---
+++ Javascript/Aulas/Aula 22.js
@@ -0,0 +1,12 @@
+const valor_padrao=0
+
+let valor=0
+
+function add(v){
+    valor+=v
+}
+add(10)
+console.log(valor)
+
+add(5)
+console.log(valor)
