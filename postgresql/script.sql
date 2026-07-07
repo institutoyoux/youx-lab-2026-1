ffa1f280c0b044a14ba123daee9865a4d4e6dab7
Maria Fernanda <maria.silva-youxlab@institutoyoux.org.br>
2026-07-07 17:39:11 -0300

    enviando atividades

--- postgresql/script.sql
+++ postgresql/script.sql
@@ -196,9 +196,24 @@ alter table cliente add idnacionalidade integer;
 alter table cliente add constraint fk_cln_idnacionalidade foreign key (idnacionalidade) references nacionalidade (idnacionalidade);
 select * from nacionalidade
 update cliente set idnacionalidade = 1 where idcliente in (1,2,3,4,6,10,11,14);
-update cliente set idnacionalidade = 2 where  idcliente in (5,7));
-update cliente set idnacionalidade = 3 where idcliente 
+update cliente set idnacionalidade = 2 where  idcliente in (5,7);
+update cliente set idnacionalidade = 3 where idcliente = 8;
+update cliente set idnacionalidade = 4 where idcliente in (9,13);
 
+select * from cliente;
+alter table cliente drop complemento;
+alter table cliente add idcomplemento integer; 
+alter table cliente add constraint fk_cln_idcomplemento foreign key (idcomplemento) references complemento (idcomplemento);
+update cliente set idcomplemento = 1 where idcliente in (1,4,9,13);
+update cliente set idcomplemento = 2 where idcliente in (2,3,7);
+
+alter table cliente drop bairro;
+alter table cliente add idbairro;
+alter table cliente add constraint foreign key (idbairro) references bairro (idbairro);
+
+update cliente set idbairro = 1 where idcliente in (1,12,13);
+update cliente set idbairro = 2 where idcliente in (2,3,6,8,9);
+update cliente set idbairro = 3 where idcliente in (1,12,13);
 
 
 
