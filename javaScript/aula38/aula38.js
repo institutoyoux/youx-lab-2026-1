dd41892801e63130bfc0c91354d3c87473270f70
Luiz Fabiano <luiz.silva-youxlab@institutoyoux.org.br>
2026-07-08 17:01:08 -0300

    Aula 38

---
+++ javaScript/aula38/aula38.js
@@ -0,0 +1,8 @@
+const caixa1=document.querySelector('#caixa1')
+const btn_c=[...document.querySelectorAll('.curso')]
+
+// console.log(caixa1.firstChild)
+// console.log(caixa1.lastChild)
+// console.log(caixa1.firstChild)
+console.log(btn_c[0].getRootNode())
+console.log(btn_c[0].ownerDocument)
