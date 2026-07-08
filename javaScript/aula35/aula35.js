c1c27939dd57c269708aefb5dbe2f10482bb6517
Luiz Fabiano <luiz.silva-youxlab@institutoyoux.org.br>
2026-07-08 14:17:25 -0300

    Aula 35

---
+++ javaScript/aula35/aula35.js
@@ -0,0 +1,31 @@
+const caixa1=document.querySelector('#caixa1')
+const caixa2=document.querySelector('#caixa2')
+const btn=document.querySelector('#btn_copiar')
+const todosCursos=[...document.querySelectorAll('.curso')]
+
+const voltar=document.querySelector('#btn_voltar')
+
+
+
+todosCursos.map((el)=>{
+    el.addEventListener('click',(evt)=>{
+        const curso=evt.target
+        curso.classList.toggle('selecionado')
+    })
+})
+
+btn.addEventListener('click',()=>{
+    const cursosSelecionados=[...document.querySelectorAll('.selecionado')]
+    cursosSelecionados.map((el)=>{
+        el.classList.toggle('selecionado')
+        caixa2.appendChild(el)
+    })
+})
+
+voltar.addEventListener('click', ()=>{
+    const cursosSelecionados=[...document.querySelectorAll('.selecionado')]
+    cursosSelecionados.map((el)=>{
+        el.classList.toggle('selecionado')
+        caixa1.appendChild(el)
+    })
+})  
