73dcd7dce67dd49f33713ad2d5fb94a217323715
João Manoel <joao.neto-youxlab@institutoyoux.org.br>
2026-07-12 03:03:25 -0300

    03 - area-do-terreno

---
+++ estudoProprio/java/desafios/03-area-do-terreno/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double largura = 12.5;
+        double comprimento = 30.0;
+        double area = largura * comprimento;
+        System.out.println("Largura: " + largura);
+        System.out.println("Comprimento: " + comprimento);
+        System.out.println("Area: " + area + " m²");
+    }
+}
