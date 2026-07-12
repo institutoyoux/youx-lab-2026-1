3bafb1d7f746c68c405f9de21bb96c5f73a1d481
João Manoel <joao.neto-youxlab@institutoyoux.org.br>
2026-07-12 02:39:16 -0300

    finalizei a seção 17 curso java (aula 198)

--- java/generics-set-map/src/application/Main.java
+++ java/generics-set-map/src/application/Main.java
@@ -5,6 +5,8 @@ import entities.LogEntry;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.Instant;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
@@ -19,8 +21,13 @@ public class Main {
             Set<LogEntry> set = new HashSet<>();
             String line = br.readLine();
             while (line != null) {
-                
+                String[] dados = line.split(" ");
+                String username = dados[0];
+                Date hora = Date.from(Instant.parse(dados[1]));
+                set.add(new LogEntry(username, hora));
+                line = br.readLine();
             }
+            System.out.println("Total users: " + set.size());
         }
         catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
