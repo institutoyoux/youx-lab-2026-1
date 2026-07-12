2ed71d903c6c0943b83cfc40bb366eccf6cc857f
João Manoel <joao.neto-youxlab@institutoyoux.org.br>
2026-07-12 02:53:49 -0300

    01 - calculadora simples (estudo proprio)

---
+++ estudoProprio/java/desafios/01-calculadora/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int primeiro = 100;
+        int segundo = 2;
+        int soma = primeiro + segundo;
+        int subtracao = primeiro - segundo;
+        int multiplicacao = primeiro * segundo;
+        int divisao = primeiro / segundo;
+        System.out.println(soma);
+        System.out.println(subtracao);
+        System.out.println(multiplicacao);
+        System.out.println(divisao);
+    }
+}
