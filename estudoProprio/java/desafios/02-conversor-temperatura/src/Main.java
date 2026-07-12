31c779eb56b91d98957ebe2d22638c9e4e34623a
João Manoel <joao.neto-youxlab@institutoyoux.org.br>
2026-07-12 02:58:04 -0300

    02 - conversor temperatura

---
+++ estudoProprio/java/desafios/02-conversor-temperatura/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double celsius = 1;
+        System.out.println("Celsius: " + celsius);
+        double fahrenheit = (celsius * 9 /5) + 32;
+        System.out.println("Fahrenheit: " + fahrenheit);
+    }
+}
