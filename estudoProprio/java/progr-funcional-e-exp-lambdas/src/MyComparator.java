ece3e2d2913c03313fc670639928efce166766d3
João Manoel <joao.neto-youxlab@institutoyoux.org.br>
2026-07-13 00:55:14 -0300

    comecei a seção 18 java (aula 201)

---
+++ estudoProprio/java/progr-funcional-e-exp-lambdas/src/MyComparator.java
@@ -0,0 +1,11 @@
+import entities.Product;
+
+import java.util.Comparator;
+
+public class MyComparator implements Comparator<Product> {
+
+    @Override
+    public int compare(Product p1, Product p2) {
+        return p1.getNome().toUpperCase().compareTo(p2.getNome().toUpperCase());
+    }
+}
