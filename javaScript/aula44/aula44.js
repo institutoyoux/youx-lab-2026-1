98edb777c95dbc0a94f0f011da6cd01f4e386dbe
Luiz Fabiano <luiz.silva-youxlab@institutoyoux.org.br>
2026-07-14 17:42:13 -0300

    Aula 44 e anteriores

---
+++ javaScript/aula44/aula44.js
@@ -0,0 +1,45 @@
+const caixaCursos = document.querySelector('#caixaCursos')
+const btn_c = [...document.querySelectorAll('.curso')]
+const c1_2 = document.querySelector('#c1_2')
+const cursos = ['HTML', 'CSS', 'Javascript', 'PHP', 'React', 'MySQL', 'ReactNative']
+const btnCursoSelecionado = document.getElementById('btnCursoSelecionado')
+const btnRemoverCurso = document.getElementById('btnRemoverCurso')
+
+cursos.map((el, chave) => {
+    const novoElemento = document.createElement('div')
+    novoElemento.setAttribute('id', 'c' + chave)
+    novoElemento.setAttribute('class', 'curso c1')
+    novoElemento.innerHTML = el
+
+    const comandos = document.createElement('div')
+    comandos.setAttribute('class', 'comandos')
+
+    const rb = document.createElement('input')
+    rb.setAttribute('type', 'radio')
+    rb.setAttribute('name', 'rb_curso')
+
+    comandos.appendChild(rb)
+
+    novoElemento.appendChild(comandos)
+
+    caixaCursos.appendChild(novoElemento)
+
+})
+
+btnCursoSelecionado.addEventListener('click', (evt) => {
+    const todosRadios = [...document.querySelectorAll('input[type=radio')]
+    let radioSelecionado = todosRadios.filter((ele, ind, arr) => {
+        return ele.checked
+    })
+    radioSelecionado=radioSelecionado[0]
+    const cursoSelecionado=radioSelecionado.parentNode.parentNode.textContent
+    alert('Curso selecionado: ' + cursoSelecionado)
+})
+
+btnRemoverCurso.addEventListener('click',(evt)=>{
+    const todosRadios = [...document.querySelectorAll('input[type=radio')]
+    let radioSelecionado = todosRadios.filter((ele, ind, arr) => {
+        return ele.checked
+    })
+    btnRemoverCurso.remove(radioSelecionado)
+})
