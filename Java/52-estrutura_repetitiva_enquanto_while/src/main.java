4337a7cb804afd84b46a1446859a047bd071b7ca
ViniciusPizzinatto <vinicius.rodrigues-youxlab@institutoyoux.org.br>
2026-07-15 16:18:58 -0300

    aula 52 estrutura repetitiva while

---
+++ Java/52-estrutura_repetitiva_enquanto_while/src/main.java
@@ -0,0 +1,14 @@
+void main() {
+    Scanner sc = new Scanner(System.in);
+
+    int x = sc.nextInt();
+
+    int soma = 0;
+    while (x != 0) {
+        soma += x;
+        x = sc.nextInt();
+    }
+
+    System.out.println(soma);
+    sc.close();
+}
