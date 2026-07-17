e8e4e0dbc9ea92d229128b852dff3cb30be9e857
ArturYouX <arthur.souza-youxlab@institutoyoux.org.br>
2026-07-17 14:37:45 -0300

    Alterando anotações de java

--- java/08-IntroducaoPOO/Exercicio3/src/main/java/application/Student.java
+++ java/08-IntroducaoPOO/Exercicio3/src/main/java/application/Student.java
@@ -3,11 +3,28 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+
+
 public class Student {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        
+        System.out.println("Digite seu nome, e as tres notas: ");
+        String name = sc.nextLine();
+        double n1 = sc.nextDouble();
+        double n2 = sc.nextDouble();
+        double n3 = sc.nextDouble();
+        double boletim = n1 + n2 + n3;
+        String situacao = " ";
+        if (boletim >= 60) {
+            situacao = "PASS";
+            System.out.printf("SITUAÇÃO: " + situacao);        }
+        else if (boletim < 60) {
+            situacao = "FAILED";
+            System.out.println("SITUAÇÃO: " + situacao);
+            System.out.printf("REPROVADO FALTANDO " + (60 - boletim) + "PTS");
+        }
+
     }
 }
