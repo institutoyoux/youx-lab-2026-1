71accfe2773c264cbd2d90bbb4c0a6a0574ffde8
ArturYouX <arthur.souza-youxlab@institutoyoux.org.br>
2026-07-17 14:13:39 -0300

    Adicionando anotações de java

--- java/08-IntroducaoPOO/Exercicio2/src/main/java/application/Employee.java
+++ java/08-IntroducaoPOO/Exercicio2/src/main/java/application/Employee.java
@@ -1,19 +1,29 @@
 package application;
+
 import java.util.Locale;
 import java.util.Scanner;
+
+import entities.Reajuste;
 
 public class Employee {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        Employee employee = new Employee();
 
-        System.out.println("Name: ");
-        String name = sc.nextLine();
-        System.out.println("Gross salary: ");
-        double salary = sc.nextDouble();
-        System.out.println("Tax: ");
-        double tax = sc.nextDouble();
+        Reajuste rjs = new Reajuste();
+
+        System.out.printf("Name: ");
+        rjs.name = sc.nextLine();
+        System.out.printf("Gross salary: ");
+        rjs.salary = sc.nextDouble();
+        System.out.printf("Tax: ");
+        rjs.tax = sc.nextDouble();
+        double netSalary = rjs.NetSalary(rjs.salary,  rjs.tax);
+        System.out.println("Employee: " + rjs.name + ", " + netSalary);
+        System.out.printf("Wich percentage to increase salary? ");
+        rjs.percentage = sc.nextDouble();
+        double IncreaseSalary = rjs.IncreaseSalary(rjs.salary,  rjs.percentage);
+        System.out.println("Updated data: " + rjs.name + ", " + IncreaseSalary);
 
         
     }
--- java/08-IntroducaoPOO/Exercicio2/src/main/java/entities/Reajuste.java
+++ java/08-IntroducaoPOO/Exercicio2/src/main/java/entities/Reajuste.java
@@ -1,15 +1,15 @@
 package entities;
 
 public class Reajuste {
-    public String name;
-    public Double salary;
-    public Double tax;
+    public String name = " ";
+    public double salary = 0;
+    public double tax = 0;
+    public double percentage = 0;
 
-    public Double NetSalary(double salary, Double tax) {
+    public double NetSalary(double salary, double tax) {
         return salary - tax;
     }
-    public void IncreaseSalary(double, percentage) {
-        this
+    public double IncreaseSalary(double netSalary, double percentage) {
+        return netSalary * (percentage / 100 + 1);
     }
-
 }
