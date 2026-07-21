1f3ba99a2bb5938de57d57868910a2f364490dca
João Manoel <joao.neto-youxlab@institutoyoux.org.br>
2026-07-21 08:39:45 -0300

    aula 293 java finalizada

--- java/spring/src/main/java/com/startjava/spring/services/UserService.java
+++ java/spring/src/main/java/com/startjava/spring/services/UserService.java
@@ -2,6 +2,7 @@ package com.startjava.spring.services;
 
 import com.startjava.spring.entities.User;
 import com.startjava.spring.repositories.UserRepository;
+import com.startjava.spring.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
     public User insert(User obj) {
         return repository.save(obj);
---
+++ java/spring/src/main/java/com/startjava/spring/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.startjava.spring.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id " + id);
+    }
+}
