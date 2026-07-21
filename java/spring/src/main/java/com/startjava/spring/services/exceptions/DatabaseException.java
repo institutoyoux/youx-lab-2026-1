5f8a26af4219d2bd91a3fe58d8cdd0f4873650a9
João Manoel <joao.neto-youxlab@institutoyoux.org.br>
2026-07-21 08:50:59 -0300

    aula 294 java finalizada

---
+++ java/spring/src/main/java/com/startjava/spring/services/exceptions/DatabaseException.java
@@ -0,0 +1,11 @@
+package com.startjava.spring.services.exceptions;
+
+import javax.xml.crypto.Data;
+
+public class DatabaseException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+}
