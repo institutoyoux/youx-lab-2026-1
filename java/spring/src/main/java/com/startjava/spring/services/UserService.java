7c15eebb0b9b1a4cb4d4f48d8d98d48b8f9a0154
João Manoel <joao.neto-youxlab@institutoyoux.org.br>
2026-07-21 08:54:56 -0300

    aula 295 java finalizada

--- java/spring/src/main/java/com/startjava/spring/services/UserService.java
+++ java/spring/src/main/java/com/startjava/spring/services/UserService.java
@@ -4,6 +4,7 @@ import com.startjava.spring.entities.User;
 import com.startjava.spring.repositories.UserRepository;
 import com.startjava.spring.services.exceptions.DatabaseException;
 import com.startjava.spring.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -38,9 +39,13 @@ public class UserService {
         }
     }
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity,obj);
-        return repository.save(entity); 
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity,obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
