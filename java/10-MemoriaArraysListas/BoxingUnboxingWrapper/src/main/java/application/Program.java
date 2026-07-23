5fc06104c62eb8f99a3fff58829da133eb85858c
ArturYouX <arthur.souza-youxlab@institutoyoux.org.br>
2026-07-23 11:53:40 -0300

    Adicionando anotações de Java

---
+++ java/10-MemoriaArraysListas/BoxingUnboxingWrapper/src/main/java/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+public class Program {
+    public static void main(String[] args) {
+
+        int x = 20;
+
+        Integer obj = x;
+
+        int y = obj * 2;
+
+        System.out.println(y);
+    }
+}
+
