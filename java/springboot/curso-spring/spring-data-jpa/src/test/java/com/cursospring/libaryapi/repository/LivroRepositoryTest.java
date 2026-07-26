9a69f9ae752fb2d4a8df386ed962b8478f7dc6cc
João Manoel <joao.neto-youxlab@institutoyoux.org.br>
2026-07-25 22:43:40 -0300

    aula 61 springboot

--- java/springboot/curso-spring/spring-data-jpa/src/test/java/com/cursospring/libaryapi/repository/LivroRepositoryTest.java
+++ java/springboot/curso-spring/spring-data-jpa/src/test/java/com/cursospring/libaryapi/repository/LivroRepositoryTest.java
@@ -26,9 +26,44 @@ class LivroRepositoryTest {
         livro.setIsbn("90887-84874");
         livro.setPreco(BigDecimal.valueOf(100));
         livro.setGenero(GeneroLivro.FICCAO);
-        livro.setTitulo("UFO");
+        livro.setTitulo("Outro Livro");
         livro.setDataPublicacao(LocalDate.of(1980, 1, 2));
-        Autor autor = autorRepository.findById(UUID.fromString("7e8dc6ff-318c-47e8-bbfc-5cb7538cb0d0")).orElse(null);
+//        Autor autor = autorRepository.findById(UUID.fromString("7e8dc6ff-318c-47e8-bbfc-5cb7538cb0d0")).orElse(null);
+        livro.setAutor(new Autor());
+        repository.save(livro);
+    }
+
+    @Test
+    void salvarAutorELivroCascadeTest(){
+        Livro livro = new Livro();
+        livro.setIsbn("90887-84874");
+        livro.setPreco(BigDecimal.valueOf(100));
+        livro.setGenero(GeneroLivro.FICCAO);
+        livro.setTitulo("Terceiro Livro");
+        livro.setDataPublicacao(LocalDate.of(1980, 1, 2));
+
+        Autor autor = new Autor();
+        autor.setNome("José");
+        autor.setNacionalidade("Brasileira");
+        autor.setDataNascimento(LocalDate.of( 1951, 1, 31));
+        autorRepository.save(autor);
+        livro.setAutor(autor);
+        repository.save(livro);
+    }
+
+    @Test
+    void salvarCascadeTest(){
+        Livro livro = new Livro();
+        livro.setIsbn("90887-84874");
+        livro.setPreco(BigDecimal.valueOf(100));
+        livro.setGenero(GeneroLivro.FICCAO);
+        livro.setTitulo("Outro Livro");
+        livro.setDataPublicacao(LocalDate.of(1980, 1, 2));
+
+        Autor autor = new Autor();
+        autor.setNome("joão");
+        autor.setNacionalidade("Brasileira");
+        autor.setDataNascimento(LocalDate.of( 1951, 1, 31));
         livro.setAutor(autor);
         repository.save(livro);
     }
