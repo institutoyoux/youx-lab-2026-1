96617c85b23154b28ed2e29b8f16e84dfc977110
João Manoel <joao.neto-youxlab@institutoyoux.org.br>
2026-07-26 12:06:10 -0300

    aula 63 springboot

--- java/springboot/curso-spring/spring-data-jpa/src/test/java/com/cursospring/libaryapi/repository/LivroRepositoryTest.java
+++ java/springboot/curso-spring/spring-data-jpa/src/test/java/com/cursospring/libaryapi/repository/LivroRepositoryTest.java
@@ -6,6 +6,7 @@ import com.cursospring.libaryapi.model.Livro;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -28,8 +29,8 @@ class LivroRepositoryTest {
         livro.setGenero(GeneroLivro.FICCAO);
         livro.setTitulo("Outro Livro");
         livro.setDataPublicacao(LocalDate.of(1980, 1, 2));
-//        Autor autor = autorRepository.findById(UUID.fromString("7e8dc6ff-318c-47e8-bbfc-5cb7538cb0d0")).orElse(null);
-        livro.setAutor(new Autor());
+        Autor autor = autorRepository.findById(UUID.fromString("a92dd57e-3846-4dfb-ac98-4857f2d1d926")).orElse(null);
+        livro.setAutor(autor);
         repository.save(livro);
     }
 
@@ -87,5 +88,13 @@ class LivroRepositoryTest {
     void deletarCascade() {
         UUID id = UUID.fromString("bcb69c00-5dce-4c7e-a996-351b96a17c8c");
         repository.deleteById(id);
+    }
+    @Test
+    @Transactional
+    void buscarLivroTest() {
+        UUID id = UUID.fromString("14e281ae-455d-41bb-ac68-a4f4929f24e1");
+        Livro livro = repository.findById(id).orElse(null);
+        System.out.println(livro.getTitulo());
+        System.out.println(livro.getAutor().getNome());
     }
 }
