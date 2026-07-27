19e982a51ca0c97dc001a9cebf424415ec4812f4
João Manoel <joao.neto-youxlab@institutoyoux.org.br>
2026-07-26 21:19:57 -0300

    aula 64 springboot

--- java/springboot/curso-spring/spring-data-jpa/src/test/java/com/cursospring/libaryapi/repository/AutorRepositoryTest.java
+++ java/springboot/curso-spring/spring-data-jpa/src/test/java/com/cursospring/libaryapi/repository/AutorRepositoryTest.java
@@ -1,11 +1,15 @@
 package com.cursospring.libaryapi.repository;
 
 import com.cursospring.libaryapi.model.Autor;
+import com.cursospring.libaryapi.model.GeneroLivro;
+import com.cursospring.libaryapi.model.Livro;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -14,6 +18,9 @@ import java.util.UUID;
 public class AutorRepositoryTest {
     @Autowired
     AutorRepository repository;
+
+    @Autowired
+    LivroRepository livroRepository;
 
     @Test
     public void salvarTest() {
@@ -55,5 +62,36 @@ public class AutorRepositoryTest {
         var id = UUID.fromString("1705e1ec-511f-4d6e-baf7-de62e4140619");
         var maria = repository.findById(id).get();
         repository.delete(maria);
+    }
+    @Test
+    void salvarAutorComLivrosTest() {
+        Autor autor = new Autor();
+        autor.setNome("Antonio");
+        autor.setNacionalidade("Americana");
+        autor.setDataNascimento(LocalDate.of(1970, 8, 5));
+
+
+        Livro livro = new Livro();
+        livro.setIsbn("12887-84874");
+        livro.setPreco(BigDecimal.valueOf(200));
+        livro.setGenero(GeneroLivro.MISTERIO);
+        livro.setTitulo("o roubo da casa assombrada");
+        livro.setDataPublicacao(LocalDate.of(1999, 1, 2));
+        livro.setAutor(autor);
+
+        Livro livro2 = new Livro();
+        livro2.setIsbn("87557-84874");
+        livro2.setPreco(BigDecimal.valueOf(140));
+        livro2.setGenero(GeneroLivro.BIOGRAFIA);
+        livro2.setTitulo("O dia de um CEO");
+        livro2.setDataPublicacao(LocalDate.of(2017, 6, 20));
+        livro2.setAutor(autor);
+
+        autor.setLivros(new ArrayList<>());
+        autor.getLivros().add(livro);
+        autor.getLivros().add(livro2);
+
+        repository.save(autor);
+//        livroRepository.saveAll(autor.getLivros());
     }
 }
