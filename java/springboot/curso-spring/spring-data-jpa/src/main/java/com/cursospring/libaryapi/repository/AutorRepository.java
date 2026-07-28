78a78e88e62663726dc55a6f28ac1148ea055f4b
João Manoel <joao.neto-youxlab@institutoyoux.org.br>
2026-07-27 21:13:59 -0300

    aula 85 springboot

--- java/springboot/curso-spring/spring-data-jpa/src/main/java/com/cursospring/libaryapi/repository/AutorRepository.java
+++ java/springboot/curso-spring/spring-data-jpa/src/main/java/com/cursospring/libaryapi/repository/AutorRepository.java
@@ -3,7 +3,11 @@ package com.cursospring.libaryapi.repository;
 import com.cursospring.libaryapi.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface AutorRepository extends JpaRepository<Autor, UUID> {
+    List<Autor> findByNome(String nome);
+    List<Autor> findByNacionalidade(String nacionalidade);
+    List<Autor> findByNomeAndNacionalidade(String nome, String nacionalidade);
 }
--- java/springboot/curso-spring/spring-data-jpa/src/main/java/com/cursospring/libaryapi/service/AutorService.java
+++ java/springboot/curso-spring/spring-data-jpa/src/main/java/com/cursospring/libaryapi/service/AutorService.java
@@ -5,6 +5,7 @@ import com.cursospring.libaryapi.repository.AutorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -27,5 +28,18 @@ public class AutorService {
 
     public void deletar(Autor autor) {
         repository.delete(autor);
+    }
+
+    public List<Autor> pesquisa(String nome, String nacionalidade) {
+        if (nome != null && nacionalidade != null) {
+            return repository.findByNomeAndNacionalidade(nome, nacionalidade);
+        }
+        if (nome != null) {
+            return repository.findByNome(nome);
+        }
+        if (nacionalidade != null) {
+            return repository.findByNacionalidade(nacionalidade);
+        }
+        return repository.findAll();
     }
 }
