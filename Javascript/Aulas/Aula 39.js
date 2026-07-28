a0f703872b5b27774aa3e86139ea7aa094b9ed1b
YouX <leonardo.alvarenga-youxlab@institutoyoux.org.br>
2026-07-28 08:33:04 -0300

    Aula 39 Javascript

---
+++ Javascript/Aulas/Aula 39.js
@@ -0,0 +1,12 @@
+const caixa=document.querySelector(".caixa")
+const btn_c1=[...document.querySelectorAll(".curso")]
+const c1_2=document.querySelector("#c1_2")
+
+console.log(c1_2.parentNode.parentNode.children[4])
+// console.log(caixa.hasChildNodes())
+// console.log(btn_c1[0].hasChildNodes())
+// console.log(btn_c1[0].children)
+//
+// console.log(btn_c1[0].children.length > 0 ? "Possui filhos" : "Não possui filhos")
+//
+// console.log(caixa.children[2].innerHTML="DJ ARANA")
