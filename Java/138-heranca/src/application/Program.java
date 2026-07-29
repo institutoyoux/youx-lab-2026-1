a3256f8720e6cf698ae07abf40b7abbf970269fe
ViniciusPizzinatto <vinicius.rodrigues-youxlab@institutoyoux.org.br>
2026-07-29 10:11:09 -0300

    aula 138 herança

---
+++ Java/138-heranca/src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import entities.BusinessAccount;
+
+public class Program {
+
+    static void main(String[] args) {
+
+        BusinessAccount account = new BusinessAccount();
+
+        account.getBalance();
+    }
+}
