65b571cc306f0033d52ee0bff60b849e267cc4c1
Luiz Fabiano <luiz.silva-youxlab@institutoyoux.org.br>
2026-08-07 16:39:00 -0300

    Aula 66

---
+++ javaScript/aula66/aula66.js
@@ -0,0 +1,24 @@
+class Npc{
+    constructor(energia){
+        this.energia=energia
+        this.alerta=false
+    }
+    info=function(){
+        console.log(`Energia: ${this.energia}`);
+        console.log(`Alerta: ${(Npc.alerta?'Sim':'Não')}`);
+        console.log('-----------------------------------------');
+    }
+    static alertar=function(){
+        Npc.alerta=true
+    }
+}
+
+const npc1=new Npc(100)
+const npc2=new Npc(80)
+const npc3=new Npc(30)
+
+Npc.alertar()
+
+npc1.info()
+npc2.info()
+npc3.info()
