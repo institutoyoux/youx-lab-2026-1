021f554dcdd0b5e27e8d0718895a024ab109fc2e
ViniciusPizzinatto <vinicius.rodrigues-youxlab@institutoyoux.org.br>
2026-08-10 17:23:10 -0300

    176 Solução do problema com interface

--- Java/176-Solucao_do_problema_com_interface/src/application/Program.java
+++ Java/176-Solucao_do_problema_com_interface/src/application/Program.java
@@ -23,9 +23,9 @@ public class Program {
         System.out.println("Entre com os dados do aluguel");
         System.out.print("Modelo do carro: ");
         String carModel = sc.nextLine();
-        System.out.println("Retirada (dd/MM/yyyy hh:mm): ");
+        System.out.print("Retirada (dd/MM/yyyy hh:mm): ");
         LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
-        System.out.println("Retorno (dd/MM/yyyy hh:mm): ");
+        System.out.print("Retorno (dd/MM/yyyy hh:mm): ");
         LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
 
         CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
