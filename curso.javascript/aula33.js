2656adf17d1f6c2381426dc4099270f33b5751a9
ana clara valerio carneiro <ana.carneiro-youxlab@institutoyoux.org.br>
2026-08-10 14:28:43 -0300

    aulas

---
+++ curso.javascript/aula33.js
@@ -0,0 +1,26 @@
+const cursosTodos=[...document.getElementsByClassName("div")]
+const divTodos=[...document.getElementsByClassName("curso")]
+const cursosC2=[...document.getElementsByClassName("c1")]
+const cursosC2=[...document.getElementsByClassName("c2")]
+const cursoEspecial=document.getElementsByClassName("c1")
+
+// // const query_divTodas=document.querySelector("div")
+// const query_divTodas=[...document.querySelectorAll("div[class]")]
+const query_divTodas=[...document.querySelectorAll("div > p")]
+const query_cursosTodos=[...document.querySelectorAll(". curso")]
+const query_cursosC1=[...document.querySelectorAll(".c1,p")]
+const query_cursosC2=[...document.querySelectorAll(".c2")]
+const query_cursoEspecial=document.querySelectorAll("#c1")[0]
+
+console.log(query_divTodas)
+// console.log(query_cursosTodos)
+
+console.log(divTodas)
+console.log(cursosTodos)
+console.log(cursosC1)
+console.log(cursosC2)
+console.log(cursoEspecial)
+
+// cursosTodos.map((el)=>{
+//     el.classList.add("destaque")
+// })
