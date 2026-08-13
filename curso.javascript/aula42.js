46d98d4351f7f3830165538c7a5b1e81e0ab171f
ana clara valerio carneiro <ana.carneiro-youxlab@institutoyoux.org.br>
2026-08-13 17:05:56 -0300

    aulas

---
+++ curso.javascript/aula42.js
@@ -0,0 +1,23 @@
+//  const filtroMaior18=(valor,indice,array)=>{
+const filtroMaior18=(valor)=>{
+    if(valor >= 18)
+        return valor
+}
+
+
+const idade=[15,21,30,17,18,44,12,50]
+const maior=idades.filter((val,ind,arr)=>{
+    if(val >= 18)
+        return val
+})
+
+
+const maior=idades.filter((val,ind,arr)=>{
+    if(val < 18)
+        return val
+})
+
+
+console.log(idades)
+console.log(maior)
+console.log(menor)
