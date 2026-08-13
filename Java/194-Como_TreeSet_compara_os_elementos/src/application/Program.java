8ccc82f1c29186e2ad5337bd3f18afda3834bfc2
ViniciusPizzinatto <vinicius.rodrigues-youxlab@institutoyoux.org.br>
2026-08-13 17:06:58 -0300

    Aula 194 Como treeSet compara os elementos

---
+++ Java/194-Como_TreeSet_compara_os_elementos/src/application/Program.java
@@ -0,0 +1,22 @@
+package application;
+
+import entities.Product;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Program {
+
+    static void main() {
+
+        Set<Product> set = new TreeSet<>();
+
+        set.add(new Product("Tv", 900.0));
+        set.add(new Product("Notebook", 1200.0));
+        set.add(new Product("Tablet", 400.0));
+
+        for (Product p : set) {
+            System.out.println(p);
+        }
+    }
+}
