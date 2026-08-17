21be11722c8eb07b0c7559c216fd37700b2d321c
ViniciusPizzinatto <vinicius.rodrigues-youxlab@institutoyoux.org.br>
2026-08-17 16:09:56 -0300

    Aula 201 Uma experiencia com Comparator

---
+++ Java/201-Uma_experiencia_com_comparator/src/application/MyComparator.java
@@ -0,0 +1,14 @@
+package application;
+
+import entities.Product;
+
+import java.util.Comparator;
+
+public class MyComparator implements Comparator<Product> {
+
+
+    @Override
+    public int compare(Product p1, Product p2) {
+        return p1.getName().toLowerCase().compareTo(p2.getName().toUpperCase());
+    }
+}
