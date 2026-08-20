09acb1db119ade05755d7e687eb97c2c712137c2
YouX <leonardo.alvarenga-youxlab@institutoyoux.org.br>
2026-08-20 13:28:40 -0300

    Aula 51 Javascript

---
+++ Javascript/Aulas/Aula 51.js
@@ -0,0 +1,20 @@
+const valores=[10,5,7,8,2,5,]
+const it_valores=valores[Symbol.iterator]()
+
+const texto="Youtube"
+const it_texto=texto[Symbol.iterator]()
+
+console.log(texto)
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+
+//Arrays
+//String
+//Map
+//Sets
