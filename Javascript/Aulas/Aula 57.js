d37ce30faf59018ebb9697c0205927064bd8cd91
YouX <leonardo.alvarenga-youxlab@institutoyoux.org.br>
2026-08-24 14:04:50 -0300

    Aula 57

---
+++ Javascript/Aulas/Aula 57.js
@@ -0,0 +1,13 @@
+class Pessoa{
+    constructor(pnome) {
+        this.nome=pnome
+    }
+}
+
+let p1=new Pessoa(1)
+let p2=new Pessoa("Sla 2")
+let p3=new Pessoa("Sla 3")
+
+console.log(p1.nome)
+console.log(p2.nome)
+console.log(p3.nome)
