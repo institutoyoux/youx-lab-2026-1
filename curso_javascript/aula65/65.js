2818d1711dac8adc6cbed9cd385a01f15198e764
Ana Clara <ana.diniz-youxlab@institutoyoux.org.br>
2026-08-24 16:31:17 -0300

    66

--- curso_javascript/aula65/65.js
+++ curso_javascript/aula65/65.js
@@ -14,7 +14,7 @@ const pessoa={
 const string_pessoa='{"nome":"Bruno","canal":"CFB Cursos","curso":"Javascript","aulas":{"aula01":"Introdução","aula02":"Variáveis","aula03":"Condicional"}}'
 
 const s_json_pessoa=JSON.stringify(pessoa) // CONVERTE OBJETO EM STRING JSON
-const s_json_pessoa=JSON.parse(s_json_pessoa) // CONVERTE STRING JSON EM OBJETO
+const o_json_pessoa=JSON.parse(s_json_pessoa) // CONVERTE STRING JSON EM OBJETO
 
 console.log(pessoa)
 console.log(s_json_pessoa)
