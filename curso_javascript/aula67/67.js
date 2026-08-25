de838fbdc52b78c3063ceb5edfc3e68a75d16370
Ana Clara <ana.diniz-youxlab@institutoyoux.org.br>
2026-08-25 14:24:30 -0300

    68

--- curso_javascript/aula67/67.js
+++ curso_javascript/aula67/67.js
@@ -1,6 +1,7 @@
 const Nave=function(energia){
     this.energia=energia
     this.disparos=100
+    this.valor=0
 }
 
 const n1=new Nave(100)
@@ -8,7 +9,8 @@ const n1=new Nave(100)
 Nave.prototype.vidas=3
 Nave.prototype.disparar=function(){
     if(this.disparos > 0){
-        this.disparos--
+        this.disparos++
+        this.valor++
     }
 }
 
@@ -18,6 +20,8 @@ n1.disparar()
 n1.disparar()
 n1.disparar()
 
+
 console.log(Nave)
 console.log(n1)
 console.log(n1.disparos)
+console.log(n1.valor)
