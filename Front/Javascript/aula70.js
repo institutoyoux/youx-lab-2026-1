9c35817aa8bfe6248936b583cdee7e9c35b88274
YouX <youx@ixnote0022.lan>
2026-08-26 15:52:35 -0300

    javaScript

---
+++ Front/Javascript/aula70.js
@@ -0,0 +1,27 @@
+const teclasNum=[...document.querySelectorAll(".num")]
+const teclasOp=[...document.querySelectorAll(".op")]
+const teclaRes=document.querySelector(".res")
+const display=document.querySelector(".display")
+const ton=document.getElementById("ton")
+const tlimpar=document.getElementById("tlimpar")
+
+teclasNum.forEach((el)=>{
+    el.addEventListener("click",(evt)=>{
+
+        if(display.innerHTML === "0"){
+            display.innerHTML = evt.target.innerHTML
+        }else{
+            display.innerHTML += evt.target.innerHTML
+        }
+
+    })
+})
+teclasOp.forEach((el)=>{
+    el.addEventListener("click",(evt)=>{
+        display.innerHTML+=evt.target.innerHTML
+    })
+})
+
+tlimpar.addEventListener("click",(evt)=>{
+    display.innerHTML="0"
+})
