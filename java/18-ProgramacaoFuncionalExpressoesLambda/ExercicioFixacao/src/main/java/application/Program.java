9d390d9589dd423ced68a7ac684dbaee441272eb
Artur Souza <arthur.souza-youxlab@institutoyoux.org.br>
2026-08-31 17:28:47 -0300

    adicionando anotações de Java

--- java/18-ProgramacaoFuncionalExpressoesLambda/ExercicioFixacao/src/main/java/application/Program.java
+++ java/18-ProgramacaoFuncionalExpressoesLambda/ExercicioFixacao/src/main/java/application/Program.java
@@ -1,12 +1,54 @@
 package application;
 
-import java.util.Scanner;
+import entities.Employee;
+import services.EmployeeService;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Program {
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        
+        System.out.print("Enter the file full path: ");
+        String path = sc.nextLine();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+            List<Employee> list = new ArrayList<>();
+
+            String line = br.readLine();
+            while (line != null) {
+                String[] fields = line.split(",");
+                list.add(new Employee(fields[0], fields[1], Double.parseDouble(fields[2])));
+                line = br.readLine();
+            }
+
+            System.out.print("Enter salary: ");
+            Double salary = sc.nextDouble();
+            System.out.println("Email of people whose salary is more than " + salary + ": ");
+
+            List<String> users = list.stream()
+                    .filter(p -> p.getSalary() > salary)
+                    .map(p -> p.getEmail())
+                    .sorted()
+                    .collect(Collectors.toList());
+            users.forEach(System.out::println);
+
+            EmployeeService es = new EmployeeService();
+
+            double sum = es.filteredSum(list, p -> p.getName().charAt(0) == 'M');
+            System.out.println("Sum of salary of people whose name starts with 'M': " + sum);
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        sc.close();
     }
 }
