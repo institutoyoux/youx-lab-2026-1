37481d1b5bd29a4a590b311891d95996b07bb7fd
Artur Souza <arthur.souza-youxlab@institutoyoux.org.br>
2026-08-31 16:49:34 -0300

    adicionando anotações de Java

---
+++ java/18-ProgramacaoFuncionalExpressoesLambda/FuncoesComoParametro/src/main/java/application/Program.java
@@ -0,0 +1,26 @@
+package application;
+
+import entities.Product;
+import model.services.ProductService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program  {
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+
+        ProductService ps = new ProductService();
+
+        double sum = ps.filteredSum(list, p -> p.getName().charAt(0) == 'T');
+        double sum2 = ps.filteredSum(list, p -> p.getPrice() < 100);
+
+        System.out.println("Sum = " + String.format("%.2f", sum));
+        System.out.println("Sum 2 = " + String.format("%.2f", sum2));
+    }
+}
