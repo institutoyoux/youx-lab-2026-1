7845d0ae2770c5eec9433f9e18b4f7d6ecd207d7
ana clara valerio carneiro <ana.carneiro-youxlab@institutoyoux.org.br>
2026-09-14 15:47:43 -0300

    aulas

--- curso.javascript/aula49.js
+++ curso.javascript/aula49.js
@@ -2,11 +2,11 @@ const p_array=document.querySelector("#array")
 const btnVerificar=document.querySelector("#btnVerificar")
 const resultado=document.querySelector("#resultado")
 
-const elementos_array=[21,25,,19,20,18,18,22]
+const elementos_array=[16,12,20,10,17,15,13,11,19]
 p_array.innerHTML="["+elementos_array+"]"
 
 btnVerificar.addEventListener("click",(evt)=>{
-    const ret=elementos_array.every((e,i)=>{
+    const ret=elementos_array.some((e,i)=>{
         if(e<18){
             resultado.innerHTML="Array não conforme na posição " + i 
         }
