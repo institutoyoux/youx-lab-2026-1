db12485a1d479f1e6c4a78844561663d52536c81
YouX <leonardo.alvarenga-youxlab@institutoyoux.org.br>
2026-09-21 13:28:33 -0300

    Aulas 70 a 73 feitas

--- Javascript/Aulas/Aula 70.js
+++
@@ -1,5 +0,0 @@
-const teclasNum=[...document.querySelectorAll(".num")]
-const taclasOp=[...document.querySelectorAll(".op")]
-const teclasRes=document.querySelector(".res")
-
-console.log(teclasOp)
---
+++ Javascript/Aulas/Aula 73.js
@@ -0,0 +1,68 @@
+const teclasNum=[...document.querySelectorAll(".num")]
+const teclasOp=[...document.querySelectorAll(".teclaOp")]
+const teclasRes=document.querySelector(".res")
+const display=document.querySelector(".display")
+const ton=document.getElementById("ton")
+const tlimpar=document.getElementById("tlimpar")
+const tigual=document.getElementById("tigual")
+const tcpy=document.getElementById("tcpy")
+const teste=document.getElementById("teste")
+
+let sinal=false
+let decima = false
+
+teclasNum.forEach((el)=>{
+    el.addEventListener("click",(evt)=>{
+        sinal=false
+        if (evt.target.innerHTML == ",") {
+            if (!decima) {
+                decima = true
+                if (display.innerHTML == "0") {
+                    display.innerHTML = "0,"
+                } else {
+                    display.innerHTML += evt.target.innerHTML
+                }
+            }
+        }else{
+            if(display.innerHTML == "0"){
+                display.innerHTML = ""
+            }
+            display.innerHTML += evt.target.innerHTML
+        }
+    })
+})
+teclasOp.forEach((el)=>{
+    el.addEventListener("click",(evt)=>{
+        if (!sinal){
+            sinal = true
+            if (display.innerHTML == "0"){
+                display.innerHTML = ""
+            }
+            if (evt.target.innerHTML == "x"){
+                display.innerHTML += "*"
+            }else {
+                display.innerHTML += evt.target.innerHTML
+            }
+        }
+    })
+})
+tlimpar.addEventListener("click",(evt)=>{
+    sinal = false
+    decima = false
+    display.innerHTML="0"
+})
+
+tigual.addEventListener("click",(evt)=>{
+    sinal = false
+    decima = false
+    const res=eval(display.innerHTML)
+    display.innerHTML=res
+})
+
+tcpy.addEventListener("click",(evt)=>{
+    // teste.select()
+    // teste.setSelectionRange(0,99999999)//Mobile
+    //navigator.clipboard.writeText(teste.value)
+    navigator.clipboard.writeText(display.innerHTML)
+})
+
