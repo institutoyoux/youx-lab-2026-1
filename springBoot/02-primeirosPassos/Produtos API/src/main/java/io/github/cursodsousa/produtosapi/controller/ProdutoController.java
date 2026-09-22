d551bd6328f3838a5b0794907e6d8092f1605376
Artur Souza <arthur.souza-youxlab@institutoyoux.org.br>
2026-09-22 14:15:28 -0300

    Adicionando anotações da aula 07 até a aula 15 do curso de Spring Boot

---
+++ springBoot/02-primeirosPassos/Produtos API/src/main/java/io/github/cursodsousa/produtosapi/controller/ProdutoController.java
@@ -0,0 +1,35 @@
+package io.github.cursodsousa.produtosapi.controller;
+
+import io.github.cursodsousa.produtosapi.model.Produto;
+import io.github.cursodsousa.produtosapi.repository.ProdutoRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
+    @PostMapping
+    public Produto salvar(@RequestBody Produto produto){
+        System.out.println("Produto recebido" + produto);
+        var id = UUID.randomUUID().toString();
+        produto.setId(id);
+
+        produtoRepository.save(produto);
+        return produto;
+    }
+
+    @GetMapping("/{id}")
+    public Produto obterPorId(@PathVariable("id") String id){
+//        Optional<Produto> produto = produtoRepository.findById(id);
+//        return produto.isPresent() ? produto.get() : null;
+        return produtoRepository.findById(id).orElse(null);
+    }
+}
