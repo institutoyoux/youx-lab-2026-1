04f60be4b7cf6fe96c5d3a59c18e7b164d839459
Lara <lara.pereira-youxlab@institutoyoux.org.br>
2026-09-24 17:54:51 -0300

    cursojavascript

---
+++ cursojavascript/aula68.js
@@ -0,0 +1,27 @@
+class Carro{
+    constructor(tipo,estagioTurbo){
+        this.turbo=new Turbo(estagioTurbo)
+        if(tipo==1){
+            this.velMax=120
+        }else if(tipo==2){
+            this.velMax=160
+        }else if(tipo==3){
+            this.velMax=200
+        }
+        this.velMax+=this.turbo
+    }
+}
+
+class Turbo{
+    constructor(e){
+        if(e==0){
+            this.pot=0
+        }else if(e==1){
+            this.pot=50
+        }else if(e==2){
+            this.pot=75
+        }else if(e==3){
+            this.pot=100
+        }
+    }
+}
